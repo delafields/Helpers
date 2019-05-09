054fef67e2c71b5b3e87bdc8b2fdd8d3208d4aac
delafields <j.c.delafields@gmail.com>
2019-05-08 20:21:33 -0400

    Added check if in US states

---
+++ state-in-US.sql
@@ -0,0 +1,3 @@
+-- is the state in the US?
+
+WHERE country_cd IN ('AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY')
